e34efd334b16f0e11b15d6423bc2cabf6d3244d2
Durbek Hayotbekov <d83217311@gmail.com>
2025-11-17 16:31:51 +0500

    Add files via upload

---
+++ formasala.py
@@ -0,0 +1,8 @@
+frends = [ "Rasulbek" , "Navrozbek" , "Farhodjon" , "Murodbek" , "Sulaymon" , "Sarvarbek" , "Bexruzbek", "Lobarxon" , "Oltinoy"]
+for x in frends:
+    if len(x) == 9 :
+        print(f"Salom , {x}")
+    elif len(x) == 8:
+        print(f"Assalomu alaykum , {x}")
+    else:
+        print(f"Salom ,{x} ahvollaring qanday ?")
