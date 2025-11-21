30c4c495da33b1acf6d43755df5c9c694bf8ba71
Durbek Hayotbekov <d83217311@gmail.com>
2025-11-21 16:26:57 +0500

    Add files via upload

---
+++ for_masalalar2.py
@@ -0,0 +1,55 @@
+# # 1
+# for y in range (1,11):
+#     print(y , end=" ")
+# # 2
+# for z in "Python":
+#     print("- " ,z)
+# # 3
+# ismlar = ["Ali", "Vali", "Dilnoza", "Madina", "Sobir"]
+# son = 0
+# for item in ismlar:
+#     print(f"{son + 1}. {item}")
+# # 4
+# for x in range(1,20 + 1):
+#     if x % 2 == 0:
+#         print(x , end=" ")
+# # 5
+# nol = 0
+# n = 50
+# for h in range(n+1):
+#     n+=h
+# print(n)
+# # 6
+# k1 = 0
+# for k in range(1,100 + 1):
+#     if k % 3 == 0:
+#         k1 += 1
+#         print(k , end=" ")
+# print(f"\nJami : {k1} ta toq son topildi")
+# # 7
+# matn = "Python dasturlash juda qiziqarli va foydali"
+# n_s = 0
+# d_z = "aeiouAEIOU"
+# for s in matn:
+#     n_s += 1
+# print(matn)
+# print(f"Unli harflar soni : {n_s}")
+# # 8
+# sonlar = [45, 12, 89, 34, 67, 23, 91, 56, 78]
+# ozgaruvchi1 = sonlar[0]
+# for h in sonlar:
+#     if h > ozgaruvchi1:
+#         ozgaruvchi1 = h
+# print(f"Eng katta son: {ozgaruvchi1}")
+# # 9
+
+
+
+
+
+
+
+
+
+
+
